647b0edfe292860246f67a578d1f6ed4ca5ffd51
ITPS <ITPS@gmail.com>
2021-12-16 17:18:57 +0900

    mybatis 설정

--- SpringStudy-junil/src/main/java/com/spring/study/web/dto/SignupReqDto.java
+++ SpringStudy-junil/src/main/java/com/spring/study/web/dto/SignupReqDto.java
@@ -1,13 +1,8 @@
 package com.spring.study.web.dto;
 
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 
-import org.mybatis.spring.mapper.MapperScannerConfigurer;
-
-@Getter
-@Setter
+@Data
 public class SignupReqDto {
 	private String username;
 	private String password;
